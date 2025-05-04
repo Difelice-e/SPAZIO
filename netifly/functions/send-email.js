9687cf79d2cdccee9c402821d981ebae9e393249
Emanuele Di Felice <difelice.emanuele@gmail.com>
2025-05-04 19:57:07 +0200

    working on

---
+++ netifly/functions/send-email.js
@@ -0,0 +1,42 @@
+const nodemailer = require("nodemailer");
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const data = JSON.parse(event.body);
+  const { email, message, lang } = data;
+
+  if (!email || !message) {
+    return { statusCode: 400, body: "Missing fields" };
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_USER, // spazio.assistance@gmail.com
+      pass: process.env.GMAIL_PASS, // app password
+    },
+  });
+
+  const mailOptions = {
+    from: `<${email}>`,
+    to: process.env.GMAIL_USER,
+    subject: `Nuovo messaggio da ${email} (${lang || "lingua non specificata"})`,
+    text: `Hai ricevuto un nuovo messaggio dal sito.\n\nEmail: ${email}\nLingua: ${lang}\n\nMessaggio:\n${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Errore nell'invio", details: error.message }),
+    };
+  }
+};
---
+++ src/js/config.js
@@ -0,0 +1,77 @@
+export const team = {
+	founder: [
+		{
+			name: "Mattia Martinelli",
+			img: "mattiamartinelli.jpg",
+			roles: ["sustainablehabitatgrad", "architectjr"],
+			nation: ["it"],
+		},
+		{
+			name: "Samuele Finocchi",
+			img: "samuelefinocchi.jpg",
+			roles: ["architect"],
+			nation: ["it"],
+		}
+	],
+	administrator: [
+		{
+			name: "Harumi Alvarado",
+			img: "harumialvarado.jpg",
+			roles: ["landscapearchitect", "mexicocoord", "socialmediamgr"],
+			nation: ["mx"],
+		},
+		{
+			name: "Rodrigo Moctezuma",
+			roles: ["architect", "latamcoord"],
+			nation: ["mx"],
+		},
+		{
+			name: "Moritz Schmail",
+			roles: ["urbanplanner", "germanycoord"],
+			nation: ["de"],
+		}
+	],
+	collaborator: [
+		{
+			name: "Jannik Krieger",
+			roles: ["urbanplanner"],
+			nation: ["de"],
+		},
+		{
+			name: "Ivanna Yáñez Solís",
+			img: "ivannasolis.jpg",
+			roles: ["architect"],
+			nation: ["mx"],
+		},
+		{
+			name: "Josué Noriega",
+			img: "josuenoriega.jpg",
+			roles: ["architect"],
+			nation: ["mx"],
+		},
+		{
+			name: "Camillo Ramos",
+			roles: ["industrialdesigner"],
+			nation: ["co"],
+		},
+		{
+			name: "Federico bonfanti",
+			roles: ["productdesigner"],
+			nation: ["it"],
+		}
+	],
+	member: [
+		{
+			name: "Carlo Alfonsetti",
+            img: "carloalfonsetti.jpg",
+			roles: ["investor"],
+			nation: ["it", "ch"],
+		},
+		{
+			name: "Emanuele Di Felice",
+            img: "emanueledifelice.jpg",
+			roles: ["webdev", "webmanager"],
+			nation: ["it"],
+		}
+	],
+};
